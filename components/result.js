5dd585508a268d9e26454506df29d76efe452419
koxyG <koxygraphics@gmail.com>
2023-04-08 21:25:03 +0100

    testing

--- components/result.js
+++ components/result.js
@@ -1,4 +1,35 @@
 // Get the canvas element
+
+//LINE CHART
+var ctx1 = document.getElementById("myChart1").getContext("2d");
+Chart.defaults.color = 'white';         
+Chart.defaults.font.size = 12;
+
+var myChart1 = new Chart(ctx1, {
+    type: 'line',
+    data: {
+        labels: ['PDP', 'APC', 'WYC', 'KPP'],
+        datasets: [{
+            label: 'Vote Chart',
+            data: [30, 50, 5, 15],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)',
+                'rgb(254, 105, 56)'
+            ],
+            hoverOffset: 4
+            
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+
+            
+//DOUGHNUT CHART
             var ctx = document.getElementById('myChart').getContext('2d');
 
             // Define the data for the chart
